5ce6653b2d318c3eb3598370ee1e55ea5380c43c
Tarek <tarekyoog@gmail.com>
2023-09-12 14:02:35 +0800

    cross-linux.cmake

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
-export CMAKE_INCLUDE_PATH="${PREFIX}/include"
-export CMAKE_LIBRARY_PATH="${PREFIX}/lib"
+#!/bin/bash
 
-mkdir build && cd build
+CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
+
+mkdir build && cd build || exit
 cmake -DCMAKE_INSTALL_PREFIX="$PREFIX" \
   -Denable_smpi=OFF \
   -DSIMGRID_PYTHON_LIBDIR="${SP_DIR}" \
+  "${CMAKE_PLATFORM_FLAGS[@]}" \
   -GNinja ..
 ninja
 ninja install

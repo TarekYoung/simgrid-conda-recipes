9e85edec269c9c00de2a871fd7f592fe69bdc137
Tarek <tarekyoog@gmail.com>
2023-09-12 18:54:32 +0800

    turn on smpi support

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
 
 mkdir build && cd build || exit
 cmake -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-  -Denable_smpi=OFF \
   -DSIMGRID_PYTHON_LIBDIR="${SP_DIR}" \
   "${CMAKE_PLATFORM_FLAGS[@]}" \
   -GNinja ..

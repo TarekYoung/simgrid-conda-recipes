e70f623af95ab0fd48ee1a79c78633084a3fc386
Tarek <tarekyoog@gmail.com>
2023-09-12 10:51:34 +0800

    update SIMGRID_PYTHON_LIBDIR

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ export CMAKE_LIBRARY_PATH="${PREFIX}/lib"
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX="$PREFIX" \
   -Denable_smpi=OFF \
-  -DSIMGRID_PYTHON_LIBDIR="${PREFIX}/lib/python${PY_VER}/site-packages" \
+  -DSIMGRID_PYTHON_LIBDIR="${SP_DIR}" \
   -GNinja ..
 ninja
 ninja install
